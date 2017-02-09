1891a242141aa9f1063fe3043993c794aac6ee05
Edouard Hur <edouard.hur@iguanesolutions.com>
2017-02-09 16:20:16 +0100

    watchdog wrapper

---
+++ watchdog/watchdog.go
@@ -0,0 +1,48 @@
+package watchdog
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/coreos/go-systemd/daemon"
+)
+
+// Controller is an interface to the systemd watchdog mechanism
+type Controller struct {
+	watchdogLimit time.Duration
+}
+
+// New returns :
+// *Controller, nil : watchdog is enabled and controller ready to be used
+// nil, nil         : watchdog is not enabled
+// nil, error       : an error occured during initialization
+func New() (*Controller, error) {
+	interval, err := daemon.SdWatchdogEnabled(false)
+	if err != nil {
+		return nil, fmt.Errorf("initialization error: %v", err)
+	}
+	if interval == 0 {
+		return nil, nil
+	}
+	return &Controller{
+		watchdogLimit: interval,
+	}, nil
+}
+
+// SendHeartbeat sends a notification to systemd watchdog
+func (c *Controller) SendHeartbeat() error {
+	sent, err := daemon.SdNotify(false, "WATCHDOG=1")
+	if err != nil {
+		return fmt.Errorf("can't send hearbeat: %v", err)
+	}
+	if !sent {
+		return fmt.Errorf("can't send hearbeat: notifications not supported (NOTIFY_SOCKET is unset)")
+	}
+	return nil
+}
+
+// GetTicker returns a ticker set at 1/3 of the watchdog duration.
+// It can be used by clients to trigger checks before using SendHeartbeat().
+func (c *Controller) GetTicker() *time.Ticker {
+	return time.NewTicker(c.watchdogLimit / 3)
+}
