57983e14d35ec74957f6e6d1b3f9506bd5a3c0fe
Edouard Hur <edouard.hur@iguanesolutions.com>
2017-03-22 15:32:17 +0100

    bring watchdog into the main package

--- watchdog.go
+++ watchdog.go
@@ -8,19 +8,19 @@ import (
 	"github.com/coreos/go-systemd/daemon"
 )
 
-// Controller is an interface to the systemd watchdog mechanism
-type Controller struct {
+// WatchDog is an interface to the systemd watchdog mechanism
+type WatchDog struct {
 	watchdogLimit time.Duration
 	sent          bool
 	err           error
 }
 
-// New returns :
+// NewWatchdog returns :
 // *Controller, nil : watchdog is enabled and controller ready to be used
 // nil, nil         : watchdog is not enabled
 // nil, error       : an error occured during initialization
-func New() (*Controller, error) {
-	c := Controller{}
+func NewWatchdog() (*WatchDog, error) {
+	c := WatchDog{}
 	c.watchdogLimit, c.err = daemon.SdWatchdogEnabled(false)
 	if c.err != nil {
 		return nil, fmt.Errorf("initialization went wrong: %v", c.err)
@@ -32,7 +32,7 @@ func New() (*Controller, error) {
 }
 
 // SendHeartbeat sends a keepalive notification to systemd watchdog
-func (c *Controller) SendHeartbeat() error {
+func (c *WatchDog) SendHeartbeat() error {
 	c.sent, c.err = daemon.SdNotify(false, "WATCHDOG=1")
 	if c.err != nil {
 		return fmt.Errorf("can't send hearbeat: %v", c.err)
@@ -45,6 +45,6 @@ func (c *Controller) SendHeartbeat() error {
 
 // NewTicker initializes and returns a ticker set at 1/3 of the watchdog duration.
 // It can be used by clients to trigger checks before using SendHeartbeat().
-func (c *Controller) NewTicker() *time.Ticker {
+func (c *WatchDog) NewTicker() *time.Ticker {
 	return time.NewTicker(c.watchdogLimit / 3)
 }
