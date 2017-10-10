cd854062f5309ebd88a56ba2fb32ced4f7a40bd1
Edouard Hur <edouard.hur@iguanesolutions.com>
2017-10-10 19:33:00 +0200

    make the watchdog dependency free (and use the notifier)

--- watchdog.go
+++ watchdog.go
@@ -1,55 +1,94 @@
 package systemd
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
-
-	"github.com/coreos/go-systemd/daemon"
 )
 
 // WatchDog is an interface to the systemd watchdog mechanism
 type WatchDog struct {
-	watchdogLimit  time.Duration
-	watchdogChecks time.Duration
-	sent           bool
-	err            error
+	notify   *Notifier
+	interval time.Duration
+	checks   time.Duration
 }
 
-// NewWatchdog returns :
-// *Controller, nil : watchdog is enabled and controller ready to be used
-// nil, nil         : watchdog is not enabled
-// nil, error       : an error occured during initialization
-func NewWatchdog() (*WatchDog, error) {
-	c := WatchDog{}
-	c.watchdogLimit, c.err = daemon.SdWatchdogEnabled(false)
-	if c.err != nil {
-		return nil, fmt.Errorf("initialization went wrong: %v", c.err)
+// NewWatchdog returns an initialized and ready to use WatchDog
+func NewWatchdog() (wd *WatchDog, err error) {
+	// Check WatchDog is supported and usable
+	interval, err := getWatchDogInterval()
+	if err != nil {
+		return
 	}
-	if c.watchdogLimit == 0 {
-		return nil, nil
+	// Spawn the systemd notifier
+	notifier, err := NewNotifier()
+	if err != nil {
+		err = fmt.Errorf("can't initialize the systemd notifier: %v", err)
+		return
 	}
-	c.watchdogChecks = c.watchdogLimit / 3
-	return &c, nil
+	// Return the initialized controller
+	wd = &WatchDog{
+		notify:   notifier,
+		interval: interval,
+		checks:   interval / 3, // 2 ?
+	}
+	return
+}
+
+// based on https://github.com/coreos/go-systemd/blob/master/daemon/watchdog.go
+func getWatchDogInterval() (interval time.Duration, err error) {
+	// WATCHDOG_USEC
+	wusec := os.Getenv("WATCHDOG_USEC")
+	if wusec == "" {
+		err = errors.New("watchdog does not seem to be enabled: WATCHDOG_USEC env unset")
+		return
+	}
+	wusecTyped, err := strconv.Atoi(wusec)
+	if err != nil {
+		err = fmt.Errorf("error converting WATCHDOG_USEC: %s", err)
+		return
+	}
+	if wusecTyped <= 0 {
+		err = fmt.Errorf("error WATCHDOG_USEC must be a positive number")
+		return
+	}
+	interval = time.Duration(wusecTyped) * time.Microsecond
+	// WATCHDOG_PID
+	wpid := os.Getenv("WATCHDOG_PID")
+	if wpid == "" {
+		return // No WATCHDOG_PID: can't check if we are the one, let's go with it
+	}
+	wpidTyped, err := strconv.Atoi(wpid)
+	if err != nil {
+		err = fmt.Errorf("error converting WATCHDOG_PID: %s", err)
+		return
+	}
+	if os.Getpid() != wpidTyped {
+		err = fmt.Errorf("WATCHDOG_PID is %d and we are %d: we are not the watched PID", wpidTyped, os.Getpid())
+	}
+	return
 }
 
 // SendHeartbeat sends a keepalive notification to systemd watchdog
 func (c *WatchDog) SendHeartbeat() error {
-	return NotifyWatchDog()
+	return c.notify.NotifyWatchDog()
 }
 
 // GetChecksDuration returns the ideal time for a client to perform (active or passive collect) checks.
-// Is is equal at 1/3 of watchdogLimit
+// Is is equal at 1/3 of watchdogInterval
 func (c *WatchDog) GetChecksDuration() time.Duration {
-	return c.watchdogChecks
+	return c.checks
 }
 
 // GetLimitDuration returns the systemd watchdog limit provided by systemd
 func (c *WatchDog) GetLimitDuration() time.Duration {
-	return c.watchdogLimit
+	return c.interval
 }
 
-// NewTicker initializes and returns a ticker set at watchdogChecks (which is set at 1/3 of watchdogLimit).
+// NewTicker initializes and returns a ticker set at watchdogChecks (which is set at 1/3 of watchdogInterval).
 // It can be used by clients to trigger checks before using SendHeartbeat().
 func (c *WatchDog) NewTicker() *time.Ticker {
-	return time.NewTicker(c.watchdogChecks)
+	return time.NewTicker(c.checks)
 }
