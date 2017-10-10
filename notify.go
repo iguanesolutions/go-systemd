539cc86a0ea1f57676c690272b6d82c035a7cb4e
Edouard Hur <edouard.hur@iguanesolutions.com>
2017-10-10 19:30:06 +0200

    move back the notifier at the pkg root

--- notify.go
+++ notify.go
@@ -3,87 +3,89 @@ package systemd
 import (
 	"errors"
 	"fmt"
-
-	"github.com/coreos/go-systemd/daemon"
+	"net"
+	"os"
 )
 
-// NotifyReady sends systemd notify READY=1
-func NotifyReady() (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, "READY=1"); !sent && err == nil {
+// Notifier wraps and facilitate systemd notify communications
+type Notifier struct {
+	socket *net.UnixAddr
+}
+
+// NewNotifier returns an initialized and ready to use Notifier if systemd notify is supported
+func NewNotifier() (notifier *Notifier, err error) {
+	// Validate Systemd notify is supported
+	socketAddr := net.UnixAddr{
+		Name: os.Getenv("NOTIFY_SOCKET"),
+		Net:  "unixgram",
+	}
+	if socketAddr.Name == "" {
 		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
+		return
+	}
+	// All good return the object
+	notifier = &Notifier{
+		socket: &socketAddr,
 	}
 	return
+}
+
+// NotifyReady sends systemd notify READY=1
+func (s *Notifier) NotifyReady() error {
+	return s.send("READY=1")
 }
 
 // NotifyReloading sends systemd notify RELOADING=1
-func NotifyReloading() (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, "RELOADING=1"); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyReloading() error {
+	return s.send("RELOADING=1")
 }
 
 // NotifyStopping sends systemd notify STOPPING=1
-func NotifyStopping() (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, "STOPPING=1"); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyStopping() error {
+	return s.send("STOPPING=1")
 }
 
 // NotifyStatus sends systemd notify STATUS=%s{status}
-func NotifyStatus(status string) (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, fmt.Sprintf("STATUS=%s", status)); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyStatus(status string) error {
+	return s.send(fmt.Sprintf("STATUS=%s", status))
 }
 
 // NotifyErrNo sends systemd notify ERRNO=%d{errno}
-func NotifyErrNo(errno int) (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, fmt.Sprintf("ERRNO=%d", errno)); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyErrNo(errno int) error {
+	return s.send(fmt.Sprintf("ERRNO=%d", errno))
 }
 
 // NotifyBusError sends systemd notify BUSERROR=%s{buserror}
-func NotifyBusError(buserror string) (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, fmt.Sprintf("BUSERROR=%s", buserror)); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyBusError(buserror string) error {
+	return s.send(fmt.Sprintf("BUSERROR=%s", buserror))
 }
 
 // NotifyMainPID sends systemd notify MAINPID=%d{mainpid}
-func NotifyMainPID(mainpid int) (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, fmt.Sprintf("MAINPID=%d", mainpid)); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyMainPID(mainpid int) error {
+	return s.send(fmt.Sprintf("MAINPID=%d", mainpid))
 }
 
 // NotifyWatchDog sends systemd notify WATCHDOG=1
-func NotifyWatchDog() (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, "WATCHDOG=1"); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
-	}
-	return
+func (s *Notifier) NotifyWatchDog() error {
+	return s.send("WATCHDOG=1")
 }
 
 // NotifyWatchDogUSec sends systemd notify WATCHDOG_USEC=%d{usec}
-func NotifyWatchDogUSec(usec int64) (err error) {
-	var sent bool
-	if sent, err = daemon.SdNotify(false, fmt.Sprintf("WATCHDOG_USEC=%d", usec)); !sent && err == nil {
-		err = errors.New("notifications are not supported (NOTIFY_SOCKET is unset)")
+func (s *Notifier) NotifyWatchDogUSec(usec int64) (err error) {
+	return s.send(fmt.Sprintf("WATCHDOG_USEC=%d", usec))
+}
+
+func (s *Notifier) send(state string) (err error) {
+	// Try to open socket
+	conn, err := net.DialUnix(s.socket.Net, nil, s.socket)
+	if err != nil {
+		err = fmt.Errorf("can't open unix socket: %v", err)
+		return
+	}
+	defer conn.Close()
+	// Write data into it
+	if _, err = conn.Write([]byte(state)); err != nil {
+		err = fmt.Errorf("can't write into the unix socket: %v", err)
 	}
 	return
 }
