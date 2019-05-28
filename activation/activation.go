326d1e35169751d6da5422cd73fa81725c12b085
Benjamin Gustin <benjamin.gustin@iguanesolutions.com>
2019-05-28 18:17:49 +0200

    allow LISTEN_FDS to be empty: support older systemd version

--- activation/activation.go
+++ activation/activation.go
@@ -44,40 +44,9 @@ func Listen(addr string) (net.Listener, error) {
 }
 
 func activationListeners() ([]net.Listener, error) {
-	if err := checkEnv(); err != nil {
+	files, err := getFiles()
+	if err != nil {
 		return nil, err
-	}
-	listenPid, err := strconv.Atoi(os.Getenv("LISTEN_PID"))
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse LISTEN_PID as int: %v", err)
-	}
-	if listenPid != os.Getpid() {
-		return nil, fmt.Errorf("bad pid (LISTEN_PID=%d, pid=%d)", listenPid, os.Getpid())
-	}
-	listenFds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse LISTEN_FDS as int: %v", err)
-	}
-	listenFdNames := strings.Split(os.Getenv("LISTEN_FDNAMES"), ":")
-	if err = os.Unsetenv("LISTEN_PID"); err != nil {
-		return nil, fmt.Errorf("failed to unset env LISTEN_PID: %v", err)
-	}
-	if err = os.Unsetenv("LISTEN_FDS"); err != nil {
-		return nil, fmt.Errorf("failed to unset env LISTEN_PID: %v", err)
-	}
-	if err = os.Unsetenv("LISTEN_FDNAMES"); err != nil {
-		return nil, fmt.Errorf("failed to unset env LISTEN_PID: %v", err)
-	}
-	files := make([]*os.File, 0, listenFds)
-	for fd := listenFdsStart; fd < listenFdsStart+listenFds; fd++ {
-		// http://man7.org/linux/man-pages/man2/fcntl.2.html
-		syscall.CloseOnExec(fd)
-		name := "LISTEN_FD_" + strconv.Itoa(fd)
-		offset := fd - listenFdsStart
-		if offset < len(listenFdNames) && len(listenFdNames[offset]) > 0 {
-			name = listenFdNames[offset]
-		}
-		files = append(files, os.NewFile(uintptr(fd), name))
 	}
 	listeners := make([]net.Listener, len(files))
 	for i, f := range files {
@@ -93,6 +62,51 @@ func activationListeners() ([]net.Listener, error) {
 	return listeners, nil
 }
 
+func getFiles() ([]*os.File, error) {
+	err := checkEnv()
+	if err != nil {
+		return nil, err
+	}
+	listenPid, err := strconv.Atoi(os.Getenv("LISTEN_PID"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse LISTEN_PID as int: %v", err)
+	}
+	if listenPid != os.Getpid() {
+		return nil, fmt.Errorf("bad pid (LISTEN_PID=%d, pid=%d)", listenPid, os.Getpid())
+	}
+	listenFds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse LISTEN_FDS as int: %v", err)
+	}
+	var listenFdNames []string
+	if _, ok := os.LookupEnv("LISTEN_FDS"); ok {
+		listenFdNames = strings.Split(os.Getenv("LISTEN_FDNAMES"), ":")
+		if err = os.Unsetenv("LISTEN_FDNAMES"); err != nil {
+			return nil, fmt.Errorf("failed to unset env LISTEN_FDNAMES: %v", err)
+		}
+	}
+	if err = os.Unsetenv("LISTEN_FDS"); err != nil {
+		return nil, fmt.Errorf("failed to unset env LISTEN_PID: %v", err)
+	}
+	if err = os.Unsetenv("LISTEN_FDNAMES"); err != nil {
+		return nil, fmt.Errorf("failed to unset env LISTEN_PID: %v", err)
+	}
+	files := make([]*os.File, 0, listenFds)
+	for fd := listenFdsStart; fd < listenFdsStart+listenFds; fd++ {
+		// http://man7.org/linux/man-pages/man2/fcntl.2.html
+		syscall.CloseOnExec(fd)
+		name := "LISTEN_FD_" + strconv.Itoa(fd)
+		if listenFdNames != nil {
+			offset := fd - listenFdsStart
+			if offset < len(listenFdNames) && len(listenFdNames[offset]) > 0 {
+				name = listenFdNames[offset]
+			}
+		}
+		files = append(files, os.NewFile(uintptr(fd), name))
+	}
+	return files, nil
+}
+
 func checkEnv() error {
 	_, ok := os.LookupEnv("LISTEN_PID")
 	if !ok {
@@ -100,9 +114,6 @@ func checkEnv() error {
 	}
 	if _, ok = os.LookupEnv("LISTEN_FDS"); !ok {
 		return errors.New("LISTEN_FDS not set")
-	}
-	if _, ok = os.LookupEnv("LISTEN_FDNAMES"); !ok {
-		return errors.New("LISTEN_FDNAMES not set")
 	}
 	return nil
 }
