6c273a6dec18cda7cc7e67a0f35165170c5db24c
Benjamin Gustin <benjamin.gustin@proteus.run>
2024-11-27 10:35:37 +0100

    handle ReplaceAttr func

--- journald/slog/handler.go
+++ journald/slog/handler.go
@@ -87,6 +87,35 @@ type Options struct {
 	AddSource bool
 	// SourceFormat specifies a function that formats the source information.
 	SourceFormat func(*slog.Source) string
+	// ReplaceAttr is called to rewrite each non-group attribute before it is logged.
+	// The attribute's value has been resolved (see [Value.Resolve]).
+	// If ReplaceAttr returns a zero Attr, the attribute is discarded.
+	//
+	// The built-in attributes with keys "time", "level", "source", and "msg"
+	// are passed to this function, except that time is omitted
+	// if zero, and source is omitted if AddSource is false.
+	//
+	// The first argument is a list of currently open groups that contain the
+	// Attr. It must not be retained or modified. ReplaceAttr is never called
+	// for Group attributes, only their contents. For example, the attribute
+	// list
+	//
+	//     Int("a", 1), Group("g", Int("b", 2)), Int("c", 3)
+	//
+	// results in consecutive calls to ReplaceAttr with the following arguments:
+	//
+	//     nil, Int("a", 1)
+	//     []string{"g"}, Int("b", 2)
+	//     nil, Int("c", 3)
+	//
+	// ReplaceAttr can be used to change the default keys of the built-in
+	// attributes, convert types (for example, to replace a `time.Time` with the
+	// integer seconds since the Unix epoch), sanitize personal information, or
+	// remove attributes from the output.
+	// Also ReplaceAttr can not be used to replace the built-in time, level and source attrs.
+	// since time is removed and level is modified to add the sd prefix.
+	// You can replace source field by using the SourceFormat funcion.
+	ReplaceAttr func(groups []string, a slog.Attr) slog.Attr
 	// Level reports the minimum record level that will be logged.
 	// The handler discards records with lower levels.
 	// If Level is nil, the handler assumes LevelInfo.
@@ -144,6 +173,10 @@ func NewHandler(opts Options) slog.Handler {
 				}
 			case slog.SourceKey:
 				a.Value = slog.StringValue(opts.SourceFormat(a.Value.Any().(*slog.Source)))
+			default:
+				if opts.ReplaceAttr != nil {
+					a = opts.ReplaceAttr(groups, a)
+				}
 			}
 			// This key does not need modification, return it as is.
 			return a
