b2cb6668dde42b8d28fb60ab00d6be86d98c116c
Benjamin Gustin <benjamin.gustin@proteus.run>
2024-11-27 10:44:28 +0100

    use default slog.HandlerOptions

--- journald/slog/handler.go
+++ journald/slog/handler.go
@@ -79,47 +79,6 @@ func GetLogLevel(raw string) slog.Leveler {
 	}
 }
 
-// Options represents the options for the journald slog handler.
-type Options struct {
-	// AddSource causes the handler to compute the source code position
-	// of the log statement and add a SourceKey attribute to the output.
-	AddSource bool
-	// ReplaceAttr is called to rewrite each non-group attribute before it is logged.
-	// The attribute's value has been resolved (see [Value.Resolve]).
-	// If ReplaceAttr returns a zero Attr, the attribute is discarded.
-	//
-	// The built-in attributes with keys "time", "level", "source", and "msg"
-	// are passed to this function, except that time is omitted
-	// if zero, and source is omitted if AddSource is false.
-	//
-	// The first argument is a list of currently open groups that contain the
-	// Attr. It must not be retained or modified. ReplaceAttr is never called
-	// for Group attributes, only their contents. For example, the attribute
-	// list
-	//
-	//     Int("a", 1), Group("g", Int("b", 2)), Int("c", 3)
-	//
-	// results in consecutive calls to ReplaceAttr with the following arguments:
-	//
-	//     nil, Int("a", 1)
-	//     []string{"g"}, Int("b", 2)
-	//     nil, Int("c", 3)
-	//
-	// ReplaceAttr can be used to change the default keys of the built-in
-	// attributes, convert types (for example, to replace a `time.Time` with the
-	// integer seconds since the Unix epoch), sanitize personal information, or
-	// remove attributes from the output.
-	// Also ReplaceAttr can not be used to replace the built-in time and level attrs
-	// since time is removed and level is modified to add the sd prefix.
-	ReplaceAttr func(groups []string, a slog.Attr) slog.Attr
-	// Level reports the minimum record level that will be logged.
-	// The handler discards records with lower levels.
-	// If Level is nil, the handler assumes LevelInfo.
-	// The handler calls Level.Level for each record processed;
-	// to adjust the minimum level dynamically, use a LevelVar.
-	Level slog.Leveler
-}
-
 // NewHandler returns a new slog handler that writes logs in a journald compatible/enhanced format.
 func NewHandler(opts slog.HandlerOptions) slog.Handler {
 	return slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
